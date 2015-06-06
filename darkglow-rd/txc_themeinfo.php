0b9e56d73c779147f0059e69820e5da80886f0d6
mambax7 <mambax7@gmail.com>
2015-06-06 07:14:08 -0400

    Original files

---
+++ darkglow-rd/txc_themeinfo.php
@@ -0,0 +1,7 @@
+<?php
+	$txc_themeinfo["name"] = "darkglow-rd";
+	$txc_themeinfo["author"] = "richardsdomain";
+	$txc_themeinfo["author_url"] = "http://www.richardsdomain.com/";
+	$txc_themeinfo["detail_url"] = "http://www.richardsdomain.com/modules/wmpdownloads/";
+	$txc_themeinfo["download_url"] = "http://www.richardsdomain.com/modules/wmpdownloads/";
+?>
